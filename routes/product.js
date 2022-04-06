db55a6564351111ba10de50e8dd74c15cdb9a011
Christian Russo <christianrusso@MacBook-Pro-de-Christian.local>
2022-04-06 15:30:47 -0300

    ada

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const PDFDocument =  require('pdfkit');
+const PDFDocument = require('pdfkit');
 const conexion = require('../database');
-var fs=require('fs');
+var fs = require('fs');
 const router = express.Router();
+const { Base64Encode } = require('base64-stream');
 
 router.post('/crear', async (req, res, next) => {
   const { descripcion, precio_base } = req.body;
@@ -18,13 +19,56 @@ router.post('/crear', async (req, res, next) => {
   );
 });
 
+function createTable(doc, data, width = 500) {
+  const startY = doc.y,
+    startX = doc.x,
+    distanceY = 15,
+    distanceX = 10;
+
+  doc.fontSize(10);
+
+  let currentY = startY;
+
+  data.forEach(value => {
+    let currentX = startX,
+      size = value.length;
+
+    let blockSize = width / size;
+
+    value.forEach(text => {
+      //Write text
+      doc.text(text, currentX + distanceX, currentY);
+
+      //Create rectangles
+      doc
+        .lineJoin("miter")
+        .rect(currentX, currentY, blockSize, distanceY)
+        .stroke();
+
+      currentX += blockSize;
+    });
+
+    currentY += distanceY;
+  });
+}
+
 router.post('/crearPDF', async (req, res, next) => {
   doc = new PDFDocument();
-  doc.pipe( fs.createWriteStream('out.pdf') );
-  doc.end();
-  res.send(200);
+  createTable(doc, [["Producto 1", 123123], ["Producto 1", 123123], ["Producto 1", 123123]], 500);
   
+  var finalString = ''; // contains the base64 string
+  var stream = doc.pipe(new Base64Encode());
+  doc.end();
+
+  stream.on('data', function (chunk) {
+    finalString += chunk;
   });
+
+  stream.on('end', function () {
+    res.json({ finalString });
+  });
+
+});
 
 router.get('/:id', (req, res, next) => {
   console.log("asdasd")
