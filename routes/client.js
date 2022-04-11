015f59bb43c5132ab3f9e132cacefe9ce532e451
lucasrusman <rusmanlucas@gmail.com>
2022-04-11 12:51:13 -0300

    Clients pdf generator created

--- routes/client.js
+++ routes/client.js
@@ -17,6 +17,71 @@ router.post('/crear', async (req, res, next) => {
     }
   );
 });
+function createTable(doc, data, width = 500) {
+  const startY = doc.y,
+    startX = doc.x,
+    distanceY = 15,
+    distanceX = 10;
+
+  doc.fontSize(10);
+
+  let currentY = startY;
+  let i = 1;
+  data.forEach(value => {
+    let currentX = startX,
+      size = value.length;
+    console.log(currentX + distanceX, currentY);
+    let blockSize = width / size;
+
+    value.forEach(text => {
+      //Write text
+      doc.text(text, currentX + distanceX, currentY);
+
+      //Create rectangles
+      doc.lineJoin('miter').rect(currentX, currentY, blockSize, distanceY).stroke();
+
+      currentX += blockSize;
+    });
+    console.log(i);
+    if (i % 40 == 0) {
+      currentY = startY;
+      doc.addPage();
+    }
+    currentY += distanceY;
+    i++;
+  });
+}
+
+router.post('/crearPDF', async (req, res, next) => {
+  const arrayClientes = [];
+  clientes = [];
+  clientes = conexion.query('SELECT * FROM clientes limit 90', function (err, rows, fields) {
+    if (!err) {
+      rows.forEach(row => {
+        allClients = [row.nombre, row.telefono, row.email, row.zona, row.direccion, row.detalle];
+        arrayClientes.push(allClients);
+      });
+      doc = new PDFDocument();
+      createTable(doc, arrayClientes, 500);
+
+      var finalString = ''; // contains the base64 string
+      var stream = doc.pipe(new Base64Encode());
+
+      doc.end();
+
+      stream.on('data', function (chunk) {
+        finalString += chunk;
+      });
+
+      stream.on('end', function () {
+        res.json({ finalString });
+      });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
   conexion.query('SELECT * FROM clientes WHERE id = ?', [id], (err, rows, fields) => {
@@ -36,7 +101,6 @@ router.get('', (req, res, next) => {
     }
   });
 });
-
 
 router.put('/:id', (req, res) => {
   const { id } = req.params;
