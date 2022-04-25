9889faa20d1785f67dceddb9397a349a1ea9de1c
lucasrusman <rusmanlucas@gmail.com>
2022-04-25 09:18:15 -0300

    Pdf export clients on sales created

--- routes/sale.js
+++ routes/sale.js
@@ -63,6 +63,208 @@ router.post('/crearPDF', async (req, res, next) => {
     }
   });
 });
+function generarExportarClientesHTML(clientes) {
+  let date_ob = new Date();
+  let date = ('0' + date_ob.getDate()).slice(-2);
+  let month = ('0' + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  console.log(clientes.length);
+
+  var html =
+    `
+  <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>A simple, clean, and responsive HTML invoice template</title>
+
+		<style>
+			.invoice-box {
+				max-width: 800px;
+				margin: auto;
+				padding: 30px;
+				border: 1px solid #eee;
+				box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+				font-size: 16px;
+				line-height: 24px;
+				font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+				color: #555;
+			}
+
+			.invoice-box table {
+				width: 100%;
+				line-height: inherit;
+				text-align: left;
+			}
+
+			.invoice-box table td {
+				padding: 5px;
+				vertical-align: top;
+			}
+
+			.invoice-box table tr td:nth-child(2) {
+				text-align: right;
+			}
+
+			.invoice-box table tr.top table td {
+				padding-bottom: 20px;
+			}
+
+			.invoice-box table tr.top table td.title {
+				font-size: 45px;
+				line-height: 45px;
+				color: #333;
+			}
+
+			.invoice-box table tr.information table td {
+				padding-bottom: 40px;
+			}
+
+			.invoice-box table tr.heading td {
+				background: #eee;
+				border-bottom: 1px solid #ddd;
+				font-weight: bold;
+			}
+
+			.invoice-box table tr.details td {
+				padding-bottom: 20px;
+			}
+
+			.invoice-box table tr.item td {
+				border-bottom: 1px solid #eee;
+			}
+
+			.invoice-box table tr.item.last td {
+				border-bottom: none;
+			}
+
+			.invoice-box table tr.total td:nth-child(2) {
+				border-top: 2px solid #eee;
+				font-weight: bold;
+			}
+
+			@media only screen and (max-width: 600px) {
+				.invoice-box table tr.top table td {
+					width: 100%;
+					display: block;
+					text-align: center;
+				}
+
+				.invoice-box table tr.information table td {
+					width: 100%;
+					display: block;
+					text-align: center;
+				}
+			}
+			.invoice-box.rtl {
+				direction: rtl;
+				font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+			}
+
+			.invoice-box.rtl table {
+				text-align: right;
+			}
+
+			.invoice-box.rtl table tr td:nth-child(2) {
+				text-align: left;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div class="invoice-box">
+			<table cellpadding="0" cellspacing="0">
+				<tr class="top">
+					<td colspan="3">
+						<table>
+							<tr>
+								<td class="title" colspan="2">
+                  <img src="http://distribuidoradhr.com.ar/assets/img/logo.jpg" style="width: 100%; max-width: 200px" />
+								</td>
+              
+								<td>
+									Fecha: ` +
+    date +
+    '-' +
+    month +
+    '-' +
+    year +
+    ` <br />
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr class="heading">
+					<td>Nombre</td>
+          			<td>-</td>
+					<td>-</td>
+				</tr>
+
+          <tr class="item">
+          `;
+  clientes.forEach(cliente => {
+    html =
+      html +
+      `<tr>
+      <td>` +
+      cliente[0] +
+      `</td>
+	  <td>-</td>
+	  <td>-</td>
+                  </tr>`;
+  });
+  html =
+    html +
+    `</tr>
+			</table>
+		</div>
+	</body>
+</html>
+`;
+  return html;
+}
+router.post('/crearPDF/exportarClientes', async (req, res, next) => {
+  ventas = req.body.sales;
+  console.log(ventas);
+  const arraySale = [];
+  let query =
+    'SELECT DISTINCT c.nombre FROM ventas v inner join clientes c on v.idCliente = c.id where v.id in (';
+	ventas.forEach(venta => {
+	console.log(venta.idCliente);
+    query = query + ' ' + venta.id + ',';
+  });
+  query = query.slice(0, -1);
+  query = query + ')';
+  console.log(query);
+  conexion.query(query, function (err, rows, fields) {
+    if (!err) {
+      rows.forEach(row => {
+        console.log(row.nombre);
+        allSales = [row.nombre];
+        arraySale.push(allSales);
+      });
+      listadoClientesPorVentaHTML = generarExportarClientesHTML(arraySale);
+      pdf
+        .create(listadoClientesPorVentaHTML)
+        .toFile('./clientesPorVenta.pdf', function (err, res2) {
+          if (err) {
+            console.log(err);
+          } else {
+            pdf2base64('./clientesPorVenta.pdf')
+              .then(response => {
+                res.status(200).json({ finalString: response });
+              })
+              .catch(error => {
+                console.log(error);
+              });
+          }
+        });
+    } else {
+      console.log(err);
+    }
+  });
+});
 function generarExportarProductosHTML(productos_por_venta) {
   let date_ob = new Date();
   let date = ('0' + date_ob.getDate()).slice(-2);
@@ -378,9 +580,9 @@ Funciones auxiliares
 */
 
 function generarVentaHTML(datosCliente, datosVenta) {
-	console.log(datosVenta)
+  console.log(datosVenta);
 
-	//ESTE
+  //ESTE
   var html =
     `
   <!DOCTYPE html>
@@ -507,16 +709,22 @@ function generarVentaHTML(datosCliente, datosVenta) {
 						<table>
 							<tr>
 								<td>
-								Cliente: `+datosCliente.nombre+`<br />
-								Telefono: `+datosCliente.telefono+`<br />
+								Cliente: ` +
+    datosCliente.nombre +
+    `<br />
+								Telefono: ` +
+    datosCliente.telefono +
+    `<br />
 								</td>
 
 								<td>
                 ` +
-				`Direccion: `+datosCliente.direccion +
+    `Direccion: ` +
+    datosCliente.direccion +
     `<br />
                 ` +
-				`Zona: `+ datosCliente.zona +
+    `Zona: ` +
+    datosCliente.zona +
     `
 								</td>
 							</tr>
