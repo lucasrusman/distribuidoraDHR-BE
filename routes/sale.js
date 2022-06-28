41a479b3d8201a1950734d8437173673831c8b4c
Facundo Rusman <rusmanfacundo@gmail.com>
2022-06-28 20:00:06 -0300

    a

--- routes/sale.js
+++ routes/sale.js
@@ -708,10 +708,10 @@ function generarVentaHTML(datosCliente, datosVenta) {
     year +
     ` <br />
 	www.dygcombos.com.ar <br />
-	<img src="https://dyg-frontend.herokuapp.com/assets/images/logo-instagram.png" style="width: 100px; max-width: 10px; height: 10px;margin-top: 2px"/>
+	<img src="https://dyg-frontend.herokuapp.com/assets/images/logo-instagram.png" style="width: 100px; max-width: 10px; height: 10px;"/>
 
 	dygcombos <br />
-	<p style="font-size: 4.5px; margin:0; text-align:end">11-6482-8268</p>
+	<p style="font-size: 4.5px; margin:0; text-align:end;margin-top: 2px">11-6482-8268</p>
 								</td>
 							</tr>
 						</table>
