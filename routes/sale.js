c630e0a2ba538cbc5d80b28f7378caba022f2192
lucasrusman <rusmanlucas@gmail.com>
2022-07-04 16:11:31 -0300

    E

--- routes/sale.js
+++ routes/sale.js
@@ -708,7 +708,7 @@ function generarVentaHTML(datosCliente, datosVenta) {
     year +
     ` <br />
 	www.dygcombos.com.ar <br />
-	<img src="https://dyg-frontend.herokuapp.com/assets/images/logo-instagram.png" style="width: 100px; max-width: 10px; height: 10px;"/>dygcombos<br />
+	<img src="https://dyg-frontend.herokuapp.com/assets/images/logo-instagram.png" style="max-width: 3px; height: 3px;"/>dygcombos<br />
 	<p style="font-size: 6px; margin:0; text-align:end;">11-6482-8268</p>
 								</td>
 							</tr>
