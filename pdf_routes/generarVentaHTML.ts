7fbf61b490a770ef889f0c05f4fea73fc80b3951
Facundo <rusmanfacundo@gmail.com>
2022-07-07 23:02:37 -0300

    mas pruebas

--- pdf_routes/generarVentaHTML.ts
+++ pdf_routes/generarVentaHTML.ts
@@ -499,7 +499,7 @@ function generarVentaHTML(datosCliente, datosVenta) {
   .invoicelist-body td,
   .invoicelist-body th {
     position: relative;
-    padding: 1rem
+    padding: 0.5rem
   }
   
   .invoicelist-body tr:nth-child(even) {
