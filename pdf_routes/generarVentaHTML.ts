c7c7510a228fede7b83ca2d7be0d5f2a2820c77f
lucasrusman <rusmanlucas@gmail.com>
2022-07-08 13:14:32 -0300

    a

--- pdf_routes/generarVentaHTML.ts
+++ pdf_routes/generarVentaHTML.ts
@@ -397,8 +397,8 @@ function generarVentaHTML(datosCliente, datosVenta) {
   }
   
   header {
-    margin: 1rem 0 0;
-    padding: 0 0 2rem 0;
+    margin: 0.5rem 0 0;
+    padding: 0 0 1rem 0;
     border-bottom: 3pt solid #009688
   }
   
