8d0eead05f83e5c161ee94aa21aa9be1e0de1806
lucasrusman <rusmanlucas@gmail.com>
2022-07-08 13:13:43 -0300

    a

--- pdf_routes/generarVentaHTML.ts
+++ pdf_routes/generarVentaHTML.ts
@@ -645,26 +645,26 @@ function generarVentaHTML(datosCliente, datosVenta) {
   
   <div class="info">
   <p >
-  <div style="  display:flex; align-items:center; justify-content:left;font-size:12px">
+  <div style="  display:flex; align-items:center; justify-content:left;font-size:13px">
     <strong>DyG Combos</strong><br>
   </div>
 
-  <div style="  display:flex; align-items:center; justify-content:left;font-size:7px">
+  <div style="  display:flex; align-items:center; justify-content:left;font-size:9px">
     www.dygcombos.com.ar<br>
   </div>
-    <div style="  display:flex; align-items:center; justify-content:left;font-size:7px">
+    <div style="  display:flex; align-items:center; justify-content:left;font-size:9px">
       <img src="https://dyg-frontend.herokuapp.com/assets/images/logo-whatsapp.png" style="width: 10px;margin-right: 3px;">
        11-6482-8268
     </div>
-    <div style="  display:flex; align-items:center; justify-content:left;font-size:7px">
+    <div style="  display:flex; align-items:center; justify-content:left;font-size:9px">
       <img src="https://dyg-frontend.herokuapp.com/assets/images/logo-whatsapp.png" style="width: 10px;margin-right: 3px;">
        7509-4514
     </div>
-    <div style="  display:flex; align-items:center; justify-content:left;font-size:7px">
+    <div style="  display:flex; align-items:center; justify-content:left;font-size:9px">
       <img src="https://dyg-frontend.herokuapp.com/assets/images/logo-whatsapp.png" style="width: 10px;margin-right: 3px;">
        7508-8828
     </div>
-    <div style="  display:flex; align-items:center; justify-content:left;font-size:7px">
+    <div style="  display:flex; align-items:center; justify-content:left;font-size:9px">
       <img src="https://dyg-frontend.herokuapp.com/assets/images/logo-instagram.png" style="width: 10px;margin-right: 3px;">
        dygcombos
     </div>
@@ -673,7 +673,7 @@ function generarVentaHTML(datosCliente, datosVenta) {
   
   <div class="bank">
     <p>
-    <div style="  display:flex; align-items:center; justify-content:left;font-size:7px">
+    <div style="  display:flex; align-items:center; justify-content:left;font-size:13px; margin-top:0.25rem">
       <strong>
         ` +
         date +
