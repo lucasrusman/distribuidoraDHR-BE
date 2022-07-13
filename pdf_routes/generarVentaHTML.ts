c17a76f24cdd579b7bfc380b428afff4351eab33
lucasrusman <rusmanlucas@gmail.com>
2022-07-13 12:00:50 -0300

    hr

--- pdf_routes/generarVentaHTML.ts
+++ pdf_routes/generarVentaHTML.ts
@@ -788,7 +788,6 @@ function generarVentaHTML(datosCliente, datosVenta) {
     </tr>
   </table>
   </div>
-  <hr>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="assets/bower_components/jquery/dist/jquery.min.js"><\/script></script>
   <script src="assets/js/main.js"></script>
