16c531383bbf40291696d4f825f4a0030d924477
lucasrusman <rusmanlucas@gmail.com>
2022-07-14 23:54:40 -0300

    volvemos un poco para atras al commit bdf3abcbeab2e5990c89b29d9969b551d5e992fd

--- pdf_routes/generarVentaHTML.ts
+++ pdf_routes/generarVentaHTML.ts
@@ -18,12 +18,13 @@ function generarVentaHTML(datosCliente, datosVenta) {
   .invoice-box {
     max-width: 800px;
     margin: auto;
-    border-top: 1px solid #CECECE;
+    padding: 30px;
+    border: 1px solid #eee;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
     font-size: 10px;
+    line-height: 24px;
     font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
     color: #555;
-    min-height:50vh;
   }
   body {
     margin: 0;
