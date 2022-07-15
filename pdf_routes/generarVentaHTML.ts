99ffa465a4e0e0f32ec2b52058fc5c15fd9751f1
lucasrusman <rusmanlucas@gmail.com>
2022-07-14 23:47:40 -0300

    correcion de la linea

--- pdf_routes/generarVentaHTML.ts
+++ pdf_routes/generarVentaHTML.ts
@@ -19,7 +19,7 @@ function generarVentaHTML(datosCliente, datosVenta) {
     max-width: 800px;
     margin: auto;
     padding: 30px;
-    border: 1px solid #eee;
+    border-top: 1px solid #CECECE;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
     font-size: 10px;
     font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
