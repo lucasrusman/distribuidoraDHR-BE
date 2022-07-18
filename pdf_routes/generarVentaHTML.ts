ec3d90bd6b0213424ffc855008c0e318a8c7efa6
lucasrusman <rusmanlucas@gmail.com>
2022-07-18 20:24:50 -0300

    color linea

--- pdf_routes/generarVentaHTML.ts
+++ pdf_routes/generarVentaHTML.ts
@@ -17,7 +17,7 @@ function generarVentaHTML(datosCliente, datosVenta) {
   .invoice-box {
     max-width: 800px;
     margin: auto;
-    border-top: 1px solid #CECECE;
+    border-top: 1px solid #AFAFAF;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
     font-size: 10px;
     font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
