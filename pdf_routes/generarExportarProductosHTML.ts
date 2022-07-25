5fc6b201dcf4bcbc62c33a0345626dc81486c85e
Facundo <rusmanfacundo@gmail.com>
2022-07-25 10:22:36 -0300

    A

--- pdf_routes/generarExportarProductosHTML.ts
+++ pdf_routes/generarExportarProductosHTML.ts
@@ -232,7 +232,7 @@ html {
 
 body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    margin: 2rem auto 0;
+    margin: 0.5rem auto 0;
     max-width: 800px;
     background: white;
     
