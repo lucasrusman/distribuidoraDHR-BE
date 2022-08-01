7f5316d04d3ccdc834b1bd28b8afe86d292943c9
Facundo <rusmanfacundo@gmail.com>
2022-08-01 09:23:43 -0300

    3600

--- routes/user.js
+++ routes/user.js
@@ -42,7 +42,7 @@ router.post('/login', (req, res, next) => {
           });
           res.status(200).json({
             token,
-            expiresIn: token.expiresIn,
+            expiresIn: 3600,
             rol,
             Status: 'Login correcto'
           });
